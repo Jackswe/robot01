ba5c3a8af366be3fde3c0ba2dbce2e430aec3a42
Jackswe <1750805291@qq.com>
2026-05-13 13:54:17 +0800

    初始化项目

---
+++ src/main/java/com/aitest/robot01/Robot01Application.java
@@ -0,0 +1,13 @@
+package com.aitest.robot01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Robot01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Robot01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/aitest/robot01/Robot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aitest.robot01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Robot01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
